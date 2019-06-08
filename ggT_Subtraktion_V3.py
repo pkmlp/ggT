63d9d4e43b79112d87e57d30767fdd67f572e9a0
pkmlp <pkmlp@pkmlp.ch>
2019-06-08 11:24:29 +0200

    update py2 to py3

--- ggT_Subtraktion_V3.py
+++ ggT_Subtraktion_V3.py
@@ -3,17 +3,19 @@
 
 
 def ggt_subtraktion(zahlA, zahlB):
-    
+
     """
-    Berechnet von zwei übergegebenen Zahlen den grössten gemeinsamen Teiler.
-    Dazu wird immer die kleinere der beiden Zahlen von dergrössere Subtrahiert,
-    bis die beiden Zahlen gleich sind. Return GGT wenn input OK, sonst -1.
+    Berechnet den grössten gemeinsamen Teiler zweier Zahlen.
+    Dazu wird die kleinere der beiden Zahlen von der grösseren
+    Subtrahiert, bis die beiden Zahlen gleich sind.
+
+    Return GGT wenn input OK, sonst -1.
     """
 
     try:
         zahlA = int(zahlA)
         zahlB = int(zahlB)
-    except:
+    except ValueError:
         return -1
 
     if zahlA < 1 or zahlB < 1:
@@ -27,25 +29,23 @@ def ggt_subtraktion(zahlA, zahlB):
     return zahlA
 
 
-
-
 #
 # Testfälle
 #
 a, b = 12, 9
-print "GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b)
+print("GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b))
 
 a, b = 36, 8
-print "GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b)
+print("GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b))
 
 a, b = 32, "A"
-print "GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b)
+print("GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b))
 
 a, b = 36, 12
-print "GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b)
+print("GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b))
 
 a, b = 7, 11
-print "GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b)
+print("GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b))
 
 a, b = 7, -11
-print "GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b)
+print("GGT von:", a, "und", b, "ist:", ggt_subtraktion(a, b))
